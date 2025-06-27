3cbc98dea5a6d4e9ff7086da4602f9428708c7c2
Your Name <your_email@example.com>
2025-06-27 15:56:18 +0545

    all font page outside folder

---
+++ Gopanyacare/meanpage/script.js
@@ -0,0 +1,24 @@
+function chooseMode(mode) {
+  if (mode === 'anonymous') {
+    window.location.href = 'safePopUP/safepopup.html';
+  } else if (mode === 'identified') {
+    window.location.href = 'loginpage/identyLogin.html';
+  }
+}
+
+let showingFirst = true;
+
+setInterval(() => {
+  const bg1 = document.getElementById('bg1');
+  const bg2 = document.getElementById('bg2');
+
+  if (showingFirst) {
+    bg1.style.opacity = 0;
+    bg2.style.opacity = 1;
+  } else {
+    bg1.style.opacity = 1;
+    bg2.style.opacity = 0;
+  }
+
+  showingFirst = !showingFirst;
+}, 4000); // 🔧 Change background every 6 seconds
